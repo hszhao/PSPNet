0241714a398c32cc500a42685c28e2679414484e
Hengshuang Zhao <zhaohsww@gmail.com>
2016-12-08 18:16:56 +0800

    Update eval_all.m

--- evaluation/eval_all.m
+++ evaluation/eval_all.m
@@ -12,11 +12,10 @@ data_name = 'ADE20K'; %set to 'VOC2012' or 'cityscapes' for relevant datasets
 switch data_name
     case 'ADE20K'
         isVal = true; %evaluation on valset
-        step = 2000; %equals to number of images divide num of GPUs in testing e.g. 500=2000/4
+        step = 500; %equals to number of images divide num of GPUs in testing e.g. 500=2000/4
         data_root = '/data2/hszhao/dataset/ADEChallengeData2016'; %root path of dataset
         eval_list = 'list/ADE20K_val.txt'; %evaluation list, refer to lists in folder 'samplelist'
-        eval_list = 'validation.txt'; %evaluation list, refer to lists in folder 'samplelist'
-        save_root = 'mc_result/ADE20K/val/pspnet50_473_g0/'; %root path to store the result image
+        save_root = 'mc_result/ADE20K/val/pspnet50_473/'; %root path to store the result image
         model_weights = 'model/pspnet50_ADE20K.caffemodel';
         model_deploy = 'prototxt/pspnet50_ADE20K_473.prototxt';
         fea_cha = 150; %number of classes
@@ -26,10 +25,9 @@ switch data_name
         data_colormap = 'color150.mat'; %color map
     case 'VOC2012'
         isVal = false; %evaluation on testset
-        step = 364;
+        step = 364; %364=1456/4
         data_root = '/data2/hszhao/dataset/VOC2012';
         eval_list = 'list/VOC2012_test.txt';
-        eval_list = 'list/test.txt';
         save_root = 'mc_result/VOC2012/test/pspnet101_473/';
         model_weights = 'model/pspnet101_VOC2012.caffemodel';
         model_deploy = 'prototxt/pspnet101_VOC2012_473.prototxt';
@@ -40,10 +38,9 @@ switch data_name
         data_colormap = 'colormapvoc.mat';
     case 'cityscapes'
         isVal = true;
-        step = 125;
+        step = 125; %125=500/4
         data_root = '/data2/hszhao/dataset/cityscapes';
         eval_list = 'list/cityscapes_val.txt';
-        eval_list = 'list/fine_val.txt';
         save_root = 'mc_result/cityscapes/val/pspnet101_713/';
         model_weights = 'model/pspnet101_cityscapes.caffemodel';
         model_deploy = 'prototxt/pspnet101_cityscapes_713.prototxt';
@@ -55,25 +52,23 @@ switch data_name
 end
 skipsize = 0; %skip serveal images in the list
 
-is_save_feat = false; %set to true if final feature map is needed
+is_save_feat = false; %set to true if final feature map is needed (not suggested for storage consuming)
 save_gray_folder = [save_root 'gray/']; %path for predicted gray image
 save_color_folder = [save_root 'color/']; %path for predicted color image
 save_feat_folder = [save_root 'feat/']; %path for predicted feature map
 scale_array = [1]; %set to [0.5 0.75 1 1.25 1.5 1.75] for multi-scale testing
-%scale_array = [0.5 0.75 1 1.25 1.5 1.75];
 mean_r = 123.68; %means to be subtracted and the given values are used in our training stage
 mean_g = 116.779;
 mean_b = 103.939;
 
 acc = double.empty;
 iou = double.empty;
-gpu_id_array = [12:15]; %multi-GPUs for parfor testing, if number of GPUs is changed, remember to change the variable 'step'
-gpu_id_array = [1]; %multi-GPUs for parfor testing, if number of GPUs is changed, remember to change the variable 'step'
+gpu_id_array = [0:3]; %multi-GPUs for parfor testing, if number of GPUs is changed, remember to change the variable 'step'
 runID = 1;
 gpu_num = size(gpu_id_array,2);
 index_array = [(runID-1)*gpu_num+1:runID*gpu_num];
 
-for i = 1:gpu_num %change 'parfor' to 'for' if singe GPU testing is used
+parfor i = 1:gpu_num %change 'parfor' to 'for' if singe GPU testing is used
   eval_sub(data_name,data_root,eval_list,model_weights,model_deploy,fea_cha,base_size,crop_size,data_class,data_colormap, ...
            is_save_feat,save_gray_folder,save_color_folder,save_feat_folder,gpu_id_array(i),index_array(i),step,skipsize,scale_array,mean_r,mean_g,mean_b);
 end
